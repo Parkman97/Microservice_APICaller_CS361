1909b5ae9ade4466c0ea00f9c9d3fa11f42f78ca
Parkman97 <Parkmans@oregonstate.edu>
2023-11-26 16:18:18 -0500

    Added Error Proofing
    Error proofing for request with not enough parameters

--- Server.py
+++ Server.py
@@ -4,13 +4,14 @@ from urllib.parse import quote
 import socket
 from Data_Parser import dataParser
 
-while True:
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-    PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
         conn, addr = s.accept()
         with conn:
             try: 
@@ -23,55 +24,59 @@ while True:
                 else:
                     link = "https://api.spaceflightnewsapi.net/v4/blogs/?"
                 
-                
-                order = [1, 4, 3, 2]
+                if len(data) != 5:
+                    conn.sendall(b'Error: Missing Parameter')
 
-                for x in order:
-                    if data[x] != '':
-                        if x == 1:
-                            link += ('has_launch=' + data[x]) #1 
+                else:
 
-                        if x == 2:
-                            if 'has_launch' not in link:
-                                link += ('news_site=' + data[x])
-                                
-                            else:
-                                link += ('&news_site=' + data[x])
-                        
-                        if x == 3: #3
-                            if 'news_site' not in link and 'has_launch' not in link:
-                                link += ('published_at_gte=' + data[x])
-                        
-                            else:
-                                link += ('&published_at_gte=' + data[x])
+                    order = [1, 4, 3, 2]
 
-                        if x == 4:
-                            if int(data[x]) > 10 or "":  #2
-                                data[x] = 10
-                            if 'limit' not in link and 'has_launch' not in link and 'published_at_gte' not in link:
-                                link += ('limit=' + data[x])
+                    for x in order:
+                        if data[x] != '':
+                            if x == 1:
+                                link += ('has_launch=' + data[x]) #1 
+
+                            if x == 2:
+                                if 'has_launch' not in link:
+                                    link += ('news_site=' + data[x])
+                                    
+                                else:
+                                    link += ('&news_site=' + data[x])
                             
-                            else:
-                                    link += ('&limit=' + data[x])
-                    else:
-                        execute_code = False
-                
-                link = link.replace(" ", "")
-                response = requests.get(link)
+                            if x == 3: #3
+                                if 'news_site' not in link and 'has_launch' not in link:
+                                    link += ('published_at_gte=' + data[x])
+                            
+                                else:
+                                    link += ('&published_at_gte=' + data[x])
 
-                if response.status_code == 200:
-                    data = response.json()
-                    dict_value = dataParser(data)
-                    user_encode_data = json.dumps(dict_value).encode('utf-8')
-                else:
-                    print(f"Error failed to get data: {response.status_code}")
-                    user_encode_data = b''  # Empty bytes
+                            if x == 4:
+                                if int(data[x]) > 10 or "":  #2
+                                    data[x] = 10
+                                if 'limit' not in link and 'has_launch' not in link and 'published_at_gte' not in link:
+                                    link += ('limit=' + data[x])
+                                
+                                else:
+                                        link += ('&limit=' + data[x])
+                        else:
+                            execute_code = False
                     
-                if user_encode_data == b'[]':
-                    conn.sendall(b'Link Not Found')
-                else:
-                    conn.sendall(user_encode_data)
-                    
+                    link = link.replace(" ", "")
+                    response = requests.get(link)
+
+                    if response.status_code == 200:
+                        data = response.json()
+                        dict_value = dataParser(data)
+                        user_encode_data = json.dumps(dict_value).encode('utf-8')
+                    else:
+                        print(f"Error failed to get data: {response.status_code}")
+                        user_encode_data = b''  # Empty bytes
+                        
+                    if user_encode_data == b'[]':
+                        conn.sendall(b'Link Not Found')
+                    else:
+                        conn.sendall(user_encode_data)
+                        
 
             except IndexError as error:
                 print('\nSection: Function to Create Instances of WebDriver\nCulprit: random.choice(ua_strings)\nIndexError: {}\n'.format(error))
@@ -84,4 +89,3 @@ while True:
                 break
 
 
-    
